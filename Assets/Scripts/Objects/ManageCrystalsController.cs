925e1e165b53d78b5bf5de5aa5fb1c4d0bbae620
John Bautista <johncamilo.bg@gmail.com>
2023-09-04 21:21:02 -0500

    Avances
    Se sube para no dañar nada

--- Assets/Scripts/Objects/ManageCrystalsController.cs
+++ Assets/Scripts/Objects/ManageCrystalsController.cs
@@ -6,25 +6,38 @@ using TMPro;
 public class ManageCrystalsController : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI counter;
-    [SerializeField] public GameObject[] crystals;
 
-    public static int collectedCrystals;
-    private int totalCristals;
+    public static int collectedCrystals { get; set; }
+    private int totalCrystals;
+    private Transform[] crystals;
+
 
     private void Start()
     {
         collectedCrystals = 0;
-        totalCristals = gameObject.transform.childCount;
-        counter.text = "0 / " + totalCristals;
+        totalCrystals = gameObject.transform.childCount;
+        counter.text = "0 / " + totalCrystals;
+        getAllChild();
     }
 
     void Update()
     {
-        if (collectedCrystals == totalCristals)
+        int indexToEnable = totalCrystals - collectedCrystals;
+        crystals[indexToEnable].gameObject.SetActive(true);
+        if (collectedCrystals == totalCrystals)
         {
-
+            Debug.Log("SIIIII GANASTE OMEEE");
         }
 
-        counter.text = collectedCrystals + " / " + totalCristals;
+        counter.text = collectedCrystals + " / " + totalCrystals;
+    }
+
+    private void getAllChild()
+    {
+        for (int i = 0; i < totalCrystals; i++)
+        {
+            crystals[i] = transform.GetChild(i);
+            crystals[i].gameObject.SetActive(false);
+        }
     }
 }
